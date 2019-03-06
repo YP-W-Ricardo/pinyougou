8c138e37d3e38b34b66969f73549df36144ab9cd
sublun <sublun@126.com>
2019-03-06 14:46:34 +0800

    1.测试   赵

--- pyg_parent/web_shop/src/main/java/cn/itcast/core/controller/GoodsController.java
+++ pyg_parent/web_shop/src/main/java/cn/itcast/core/controller/GoodsController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/goods")
 public class GoodsController {
-
     @Reference
     private GoodsService goodsService;
 
